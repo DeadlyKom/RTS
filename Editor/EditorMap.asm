e940ad128ca8843f4c431172ad18e00c48895e44
DeadlyKom <gma.deadly.kom@mail.ru>
2021-04-07 02:03:35 +0300

    - новая ветка редактора

--- Editor/EditorMap.asm
+++ Editor/EditorMap.asm
@@ -2,8 +2,10 @@
                 define _EDITOR_
 
                 include "../Core/Memory/Include.inc"
-Editor:         LD HL, MemoryPage_0.MenuSprites + 10 
+Editor:         LD HL, MemoryPage_0.MenuSprites + 10
+
                 CALL MemoryPage_2.DisplayTileMap;  печать экрана
+
                 JP Editor
 
                 endif ; ~_INPUT_KEYBOARD_INCLUDE_
