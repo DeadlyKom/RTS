aa81ea9dad660f3c197222772a1b89d2ce184109
SLIPing <sergej.tackida@yandex.ru>
2021-04-07 19:42:52 +0300

    123

--- Editor/EditorMap.asm
+++ Editor/EditorMap.asm
@@ -5,7 +5,7 @@
 Editor:         LD HL, MemoryPage_0.MenuSprites + 10
                 
                 CALL MemoryPage_2.DisplayTileMap;  печать экрана
-
+                nop
                 JP Editor
 
                 endif ; ~_INPUT_KEYBOARD_INCLUDE_
