5f884b8ed0b3f5af0d557807ac8c10908f6f4202
DeadlyKom <gma.deadly.kom@mail.ru>
2021-05-09 19:15:10 +0300

    - добавлены спрайты песка (4 шт) - добавлены спрайты каньёна (16 шт)

--- Core/Memory/Page_00.asm
+++ Core/Memory/Page_00.asm
@@ -8,9 +8,24 @@
                 module MemoryPage_0
 Start:
                 align 256
-TableSprites:   DW Sprite1, Sprite2
+TableSprites:   DW Sprite_Sand_1, Sprite_Sand_2, Sprite_Sand_3, Sprite_Sand_4               ; 0  - 3
+                DW #0000, #0000, #0000, #0000                                               ; 4  - 7
+                DW #0000, #0000, #0000, #0000                                               ; 8  - 11
+                DW #0000, #0000, #0000, #0000                                               ; 12 - 15
+                DW #0000, #0000, #0000, #0000                                               ; 16 - 19
+                DW #0000, #0000, #0000, #0000                                               ; 20 - 23
+                DW #0000, Sprite1, Sprite2, #0000                                           ; 24 - 27
+                DW #0000, #0000, #0000, #0000                                               ; 28 - 31
+                DW Sprite_Canyon_1, Sprite_Canyon_2, Sprite_Canyon_3, Sprite_Canyon_4       ; 32 - 35
+                DW Sprite_Canyon_5, Sprite_Canyon_6, Sprite_Canyon_7, Sprite_Canyon_8       ; 36 - 39
+                DW Sprite_Canyon_9, Sprite_Canyon_10, Sprite_Canyon_11, Sprite_Canyon_12    ; 40 - 43
+                DW Sprite_Canyon_13, Sprite_Canyon_14, Sprite_Canyon_15, Sprite_Canyon_16   ; 44 - 47
+                DW #0000, #0000, #0000, #0000                                               ; 48 - 51
+                DW #0000, #0000, #0000, #0000                                               ; 52 - 55
+                DW #0000, #0000, #0000, #0000                                               ; 56 - 59
+                DW #0000, #0000, #0000, #0000                                               ; 60 - 63
 
-                 align 256
+                align 256
 TableFOW:       DW SpriteFOW_1, SpriteFOW_2, SpriteFOW_3, SpriteFOW_4, SpriteFOW_5, SpriteFOW_6, SpriteFOW_7, SpriteFOW_8
                 DW SpriteFOW_9, SpriteFOW_A, SpriteFOW_B, SpriteFOW_C, SpriteFOW_D, SpriteFOW_E, SpriteFOW_F
 
@@ -61,6 +76,50 @@ SpriteFOW_F     DW #1FF8, #E007, #03C0, #8001, #0180, #0000, #0000, #0000       
 
 MenuSprites:
                 incbin "../../Data/MenuSprite.bin"
+
+                ; спрайты песка
+Sprite_Sand_1:        
+                incbin "Sprites/Terrain/Sand_1.spr"
+Sprite_Sand_2:        
+                incbin "Sprites/Terrain/Sand_2.spr"
+Sprite_Sand_3:        
+                incbin "Sprites/Terrain/Sand_3.spr"
+Sprite_Sand_4:        
+                incbin "Sprites/Terrain/Sand_4.spr"
+
+                ; спрайты каньёна
+Sprite_Canyon_1:        
+                incbin "Sprites/Terrain/Canyon_1.spr"
+Sprite_Canyon_2:        
+                incbin "Sprites/Terrain/Canyon_2.spr"
+Sprite_Canyon_3:        
+                incbin "Sprites/Terrain/Canyon_3.spr"
+Sprite_Canyon_4:        
+                incbin "Sprites/Terrain/Canyon_4.spr"
+Sprite_Canyon_5:        
+                incbin "Sprites/Terrain/Canyon_5.spr"
+Sprite_Canyon_6:        
+                incbin "Sprites/Terrain/Canyon_6.spr"
+Sprite_Canyon_7:        
+                incbin "Sprites/Terrain/Canyon_7.spr"
+Sprite_Canyon_8:        
+                incbin "Sprites/Terrain/Canyon_8.spr"
+Sprite_Canyon_9:        
+                incbin "Sprites/Terrain/Canyon_9.spr"
+Sprite_Canyon_10:        
+                incbin "Sprites/Terrain/Canyon_10.spr"
+Sprite_Canyon_11:        
+                incbin "Sprites/Terrain/Canyon_11.spr"
+Sprite_Canyon_12:        
+                incbin "Sprites/Terrain/Canyon_12.spr"
+Sprite_Canyon_13:        
+                incbin "Sprites/Terrain/Canyon_13.spr"
+Sprite_Canyon_14:        
+                incbin "Sprites/Terrain/Canyon_14.spr"
+Sprite_Canyon_15:        
+                incbin "Sprites/Terrain/Canyon_15.spr"
+Sprite_Canyon_16:        
+                incbin "Sprites/Terrain/Canyon_16.spr"
 End:
                 endmodule
 SizePage_0:     EQU MemoryPage_0.End - MemoryPage_0.Start
