c419f126c4e772139d98aab5f41c4e91f96fbe21
DeadlyKom <gma.deadly.kom@mail.ru>
2021-06-12 17:01:44 +0300

    - сохранение

--- Core/Module/AI/Composite/Handler.asm
+++ Core/Module/AI/Composite/Handler.asm
@@ -10,19 +10,34 @@
 ;   HL, DE, BC, AF, AF'
 ; -----------------------------------------
 HandlerRoot:    LD DE, .RET
-                LD A, (HL)
+                PUSH DE
+                LD DE, .Continue
+.Next           LD A, (HL)
+                AND %11000000
+                JR Z, .Back
                 INC HL
                 LD C, (HL)
-                ; INC HL
-                ; PUSH HL
-                LD B, #00
-                ADD HL, BC
-                AND %11000000
-                JP Z, AI.Composite.Selector      ; BT_SELECTOR
+                PUSH DE
+                PUSH HL
+                ; AND %11000000
+                SUB #40
+                JP Z, AI.Composite.Selector         ; BT_SELECTOR
                 SUB #40
                 JP Z, AI.Composite.Sequence         ; BT_SEQUENCE
-                SUB #40
-                JP Z, AI.Composite.TaskExecute      ; BT_TASK
-.RET            RET                                 ; BT_BREAK
+                ; SUB #40
+                JP AI.Composite.TaskExecute      ; BT_TASK
+                ; BT_BREAK
+                ; EX (SP), HL
+
+.RET            RET
+.Back           POP HL
+                ; INC HL
+                EX (SP), HL
+                PUSH HL
+                RET
+
+.Continue       POP HL
+                INC HL
+                JR .Next
 
                 endif ; ~ _CORE_MODULE_AI_COMPOSITE_HANDLER_
