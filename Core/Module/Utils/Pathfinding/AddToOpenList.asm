7b7b9491f7386d368ac80c8974e6b255da0d5ed9
Mikhail Gamaev <for.work.gma@gmail.com>
2021-11-21 01:53:25 +0300

    - добавлен метод получения указатель на структуру FPFInfo в буфере - небольшая пометка удаления старого поиска пути

---
+++ Core/Module/Utils/Pathfinding/AddToOpenList.asm
@@ -0,0 +1,19 @@
+
+                ifndef _CORE_MODULE_UTILS_PATHFINDING_ADD_TO_OPEN_LIST_
+                define _CORE_MODULE_UTILS_PATHFINDING_ADD_TO_OPEN_LIST_
+
+                module Pathfinding
+; -----------------------------------------
+; put on the open list if better
+; In:
+; Out:
+; Corrupt:
+; Note:
+; -----------------------------------------
+AddToOpenList:  ;
+
+                RET
+
+                endmodule
+
+                endif ; ~ _CORE_MODULE_UTILS_PATHFINDING_ADD_TO_OPEN_LIST_
---
+++ Core/Module/Utils/Pathfinding/GetTileInfo.asm
@@ -0,0 +1,53 @@
+
+                ifndef _CORE_MODULE_UTILS_PATHFINDING_GET_TILE_INFO_
+                define _CORE_MODULE_UTILS_PATHFINDING_GET_TILE_INFO_
+
+                module Pathfinding
+; -----------------------------------------
+; get pointer to FPFInfo structure in the buffer
+; In:
+;   DE - позиция тайла (D - y, E - x)
+; Out:
+;   HL - Pointer to FPFInfo structure in buffer
+; Corrupt:
+;   HL, AF
+; Note:
+; -----------------------------------------
+GetTileInfo:    ;
+.BufferStart    EQU $+1
+                LD HL, #0000               
+
+                ; X = (Coord.x - BufferStart.x) & 0x0F
+                LD A, E
+                SUB L
+                LD L, A
+
+                ; Y = (Coord.y - BufferStart.y) & 0x0F
+                LD A, D
+                SUB H
+
+                ; HL = #4000 | Y << 4 | X
+                ;   +----+----+----+----+----+----+----+----+   +----+----+----+----+----+----+----+----+
+                ;   | 15 | 14 | 13 | 12 | 11 | 10 |  9 |  8 |   |  7 |  6 |  5 |  4 |  3 |  2 |  1 |  0 |
+                ;   +----+----+----+----+----+----+----+----+   +----+----+----+----+----+----+----+----+
+                ;   |  0 |  1 |  0 |  0 |  0 |  0 |  0 |  0 |   |  Y |  Y |  Y |  Y |  X |  X |  X |  X |
+                ;   +----+----+----+----+----+----+----+----+   +----+----+----+----+----+----+----+----+
+
+                ; Y << 4
+                ADD A, A
+                ADD A, A
+                ADD A, A
+                ADD A, A
+
+                ; Y << 4 | X
+                ADD A, L
+
+                ; HL = #4000 | Y << 4 | X
+                LD L, A
+                LD H, #40
+
+                RET
+
+                endmodule
+
+                endif ; ~ _CORE_MODULE_UTILS_PATHFINDING_GET_TILE_INFO_
