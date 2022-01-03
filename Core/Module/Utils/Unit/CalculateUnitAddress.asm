3ba438dfe8dad534852724bb5e4325e895d1b34b
Mikhail Gamaev <for.work.gma@gmail.com>
2022-01-04 02:26:34 +0300

    - фикс расчёта адреса юнита

--- Core/Module/Utils/Unit/CalculateUnitAddress.asm
+++ Core/Module/Utils/Unit/CalculateUnitAddress.asm
@@ -11,14 +11,33 @@
 ; Note:
 ; -----------------------------------------
 GetAdrUnit:     ; расчёт смещения по индексу юнита
-                OR #80
-                CCF
-                RRA
+                LD HL, .Value
+                LD (HL), A
+                XOR A
+                RLD
+                RL (HL)
+                ADC A, A
+                OR #C0
                 LD IXH, A
-                LD A, #00
-                RLA
+                LD A, (HL)
                 LD IXL, A
-                
+
+                ; портит регистр E
+                ; OR A
+                ; RRA
+                ; LD E, #00
+                ; RR E
+                ; RRA
+                ; RR E
+                ; RRA
+                ; RR E
+                ; OR #C0
+                ; LD IXH, A
+                ; LD A, E
+                ; LD IXL, A
+
                 RET
+
+.Value          DB #00
 
                 endif ; ~ _CORE_MODULE_UTILS_GET_ADDRESS_UNIT_
